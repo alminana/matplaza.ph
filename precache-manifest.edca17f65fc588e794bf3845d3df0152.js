d8e919435209b2586bf688026fc663b0ec05355a
alminana <alminanaeilerbutad@yahoo.com>
2020-06-28 05:48:53 -0700

    Updates

---
+++ precache-manifest.edca17f65fc588e794bf3845d3df0152.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b31c012725ad93630cb4c9bc866012",
+    "url": "/matplaza.ph/index.html"
+  },
+  {
+    "revision": "61ec6aafd5bd3901d1e9",
+    "url": "/matplaza.ph/static/css/main.af765c5e.chunk.css"
+  },
+  {
+    "revision": "9b54a8e6693471bddbe2",
+    "url": "/matplaza.ph/static/js/2.8e2876cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/matplaza.ph/static/js/2.8e2876cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ec6aafd5bd3901d1e9",
+    "url": "/matplaza.ph/static/js/main.aa2c828c.chunk.js"
+  },
+  {
+    "revision": "cedfb9e0a1f8e52d6dfe",
+    "url": "/matplaza.ph/static/js/runtime-main.ca0765e3.js"
+  },
+  {
+    "revision": "e114e2ec82c15d2509f0ff963f79af3e",
+    "url": "/matplaza.ph/static/media/ARCHITECT.e114e2ec.jpg"
+  },
+  {
+    "revision": "4b00a2df8cbbc8eb37b210e16903d964",
+    "url": "/matplaza.ph/static/media/PRESIDENT.4b00a2df.JPG"
+  },
+  {
+    "revision": "c55fe90793423edafdac741414dbbb29",
+    "url": "/matplaza.ph/static/media/PVC.c55fe907.jpg"
+  },
+  {
+    "revision": "4d61a2f3ec7485be19ca5659458224e3",
+    "url": "/matplaza.ph/static/media/Samsung.4d61a2f3.jpg"
+  },
+  {
+    "revision": "2684680a307224d1edbcc163426b18a1",
+    "url": "/matplaza.ph/static/media/Sanitary.2684680a.JPG"
+  },
+  {
+    "revision": "94c0d5a96a46cebe33cc5d59db0a51e7",
+    "url": "/matplaza.ph/static/media/VICE.94c0d5a9.JPG"
+  },
+  {
+    "revision": "31d645dba72e5ef3cda0bcd1c0052b4c",
+    "url": "/matplaza.ph/static/media/banner.31d645db.JPG"
+  },
+  {
+    "revision": "4ee713d250a7a5cd8082afe947b27bb6",
+    "url": "/matplaza.ph/static/media/bayantreesoul.4ee713d2.jpg"
+  },
+  {
+    "revision": "37818295604d9686d50990e480f3d679",
+    "url": "/matplaza.ph/static/media/board.37818295.jpg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/matplaza.ph/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/matplaza.ph/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/matplaza.ph/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/matplaza.ph/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/matplaza.ph/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "69c0f682dc6423b548e0eaf09d4c2f68",
+    "url": "/matplaza.ph/static/media/bulb.69c0f682.png"
+  },
+  {
+    "revision": "07d008a83514d874acd6541e29aa8852",
+    "url": "/matplaza.ph/static/media/eye.07d008a8.png"
+  },
+  {
+    "revision": "638361fefae8fa87abc308af24ec770b",
+    "url": "/matplaza.ph/static/media/firedoor.638361fe.JPG"
+  },
+  {
+    "revision": "31d645dba72e5ef3cda0bcd1c0052b4c",
+    "url": "/matplaza.ph/static/media/furniture.31d645db.jpg"
+  },
+  {
+    "revision": "fb033ec087ef81657e0e4e5f9051c226",
+    "url": "/matplaza.ph/static/media/home_automation.fb033ec0.jpg"
+  },
+  {
+    "revision": "a34d7dfdcce7331dc528b60eb0bd4567",
+    "url": "/matplaza.ph/static/media/huosung.a34d7dfd.JPG"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/matplaza.ph/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/matplaza.ph/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "69f7788ef92b3a24603ca881fa88eeeb",
+    "url": "/matplaza.ph/static/media/office.69f7788e.jpg"
+  },
+  {
+    "revision": "91ecccc41c5c16e6764cb5c801b50cb8",
+    "url": "/matplaza.ph/static/media/stone.91ecccc4.jpg"
+  },
+  {
+    "revision": "41e3ea7adc1115aaf5d98d160e8d8b33",
+    "url": "/matplaza.ph/static/media/target.41e3ea7a.png"
+  },
+  {
+    "revision": "812c895584e0aafdaa1d784223e07652",
+    "url": "/matplaza.ph/static/media/terracotta.812c8955.jpg"
+  },
+  {
+    "revision": "b59180894191dbb1f76adbf788fb7318",
+    "url": "/matplaza.ph/static/media/tile.b5918089.JPG"
+  }
+]);
